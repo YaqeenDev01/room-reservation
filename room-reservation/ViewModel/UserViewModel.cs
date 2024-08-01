9c1ef1caf598815833170b217dc9ab2c094726bc
ShathaMK <Miss.ruva.22@hotmail.com>
2024-08-01 11:22:07 +0300

    Added users views(create, Edit, Delete, Index), view model , controller and domain

---
+++ room-reservation/ViewModel/UserViewModel.cs
@@ -0,0 +1,37 @@
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace room_reservation.ViewModel
+{
+    public class UserViewModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = " هذا الحقل مطلوب")]
+        [DisplayName("الأسم الكامل بالعربي")]
+        public string FullNameAR { get; set; }
+
+        [Required(ErrorMessage = " هذا الحقل مطلوب")]
+        [DisplayName("الأسم الكامل بالإنجليزي")]
+        public string FullNameEN { get; set; }
+
+        [Required(ErrorMessage = " هذا الحقل مطلوب")]
+        [DisplayName("البريد الإلكتروني")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = " هذا الحقل مطلوب")]
+        [DisplayName("رقم الجوال")]
+        public string PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = " هذا الحقل مطلوب")]
+        [DisplayName("كلمة المرور")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = " هذا الحقل مطلوب")]
+        [DisplayName("نوع المستخدم")]
+        public string UserType { get; set; }
+       
+
+
+    }
+}
