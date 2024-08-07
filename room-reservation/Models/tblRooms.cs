3453acc83faed9ef6f28e4fece87d3fea401fccf
sarah <sarah1038@icloud.com>
2024-08-07 11:48:19 +0300

    Bookings

--- room-reservation/Models/tblRooms.cs
+++ room-reservation/Models/tblRooms.cs
@@ -4,22 +4,25 @@ namespace room_reservation.Models
 {
     public class tblRooms
     {
-        public int Id { get; set; }
+       public int Id { get; set; }   
+        public int FloorId { get; set; }   
         public int RoomNo { get; set; }
+        public int RoomTypeId { get; set; }
         public int SeatCapacity { get; set; }
         public bool IsActive { get; set; }
         public Guid guid { get; set; } 
         public bool IsDeleted { get; set; }
         public tblFloors Floor { get; set; }
-        public int FloorId { get; set; }
+       public tblRoomType  RoomType { get; set; }
 
-        public tblRoomType  RoomType { get; set; }
+        //public ICollection<tblFloors> FloorCollection { get; set; }
 
-        public int RoomTypeId { get; set; }
-        public int BuildingId { get; set; }
+
+      
         public ICollection<tblFloors> Floors { get; set; }
 
         public ICollection<tblRoomType> RoomTypes { get; set; }
+
     }
 
 }
